a026c4228a0f98acd9e19acc39e6eef3191dca0c
treasure2014 <treasure2014@treasure2014.(none)>
2014-09-27 21:13:48 +0900

    dbupを追加

---
+++ sql/V100__create_a.sql
@@ -0,0 +1,9 @@
+CREATE TABLE posts (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50),
+    body TEXT,
+    `photo` varchar(255) DEFAULT NULL,
+    `photo_dir` varchar(255) DEFAULT NULL,
+    created DATETIME DEFAULT NULL,
+    modified DATETIME DEFAULT NULL
+);
