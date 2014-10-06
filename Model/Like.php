2d6de7d6713f0df939c0d950d10653a5f9713876
root <root@www7338uo.sakura.ne.jp>
2014-10-06 16:35:07 +0900

    #14 matchingのバグを修正

--- Model/Like.php
+++ Model/Like.php
@@ -8,23 +8,24 @@ class Like extends AppModel {
     // ユーザ同士がマッチングしているかどうかを調べる
     public function isMatchUsers($user_id1, $user_id2) {
 
-        $c = $this->find('count', 
+        $count1 = $this->find('count', 
             array('conditions' => 
-                array("OR" => 
-                    array(
-                        array (
-                            'Like.send_user_id' => $user_id1,
-                            'Like.receive_user_id' => $user_id2
-                        ),
-                        array (
-                            'Like.send_user_id' => $user_id2,
-                            'Like.receive_user_id' => $user_id1
-                        )
-                    )
-                )
+                 array (
+                     'Like.send_user_id' => $user_id1,
+                     'Like.receive_user_id' => $user_id2
+                 )
             )
         );
-        
-        return ($c >= 2) ? true : false;
+
+        $count2 = $this->find('count', 
+            array('conditions' => 
+                 array (
+                     'Like.send_user_id' => $user_id2,
+                     'Like.receive_user_id' => $user_id1
+                 )
+            )
+        );
+       
+        return ($count1 > 0 && $count2 > 0) ? true : false;
     }
 }
